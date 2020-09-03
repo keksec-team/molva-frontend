4dfaa7731d597c9f0915b7b593e0264f3d7abee3
Georgii <corradowave1@gmail.com>
2020-09-03 12:53:21 +0300

    slider works

--- src/component/app/main/control/navbar/Navbar.js
+++ src/component/app/main/control/navbar/Navbar.js
@@ -9,16 +9,16 @@ import Menu from "./menu/Menu";
 function Navbar(props) {
     const {dispatch} = props;
     let {width} = useWindowSize();
-    console.log(width)
     return (
         <div className={styles.nav}>
             <label className={styles.toggleLabel}
                    onClick={() => changeNavbarToggled(!props.isToggled, dispatch)}>&#9776;</label>
-            {width <= 600 ? (
+            {(width <= 600)
+                ? (
                     <Slide direction="left" in={props.isToggled}>
-                        <Menu/>
-                    </Slide>) :
-                (<Menu/>)}
+                        <div><Menu/></div>
+                    </Slide>)
+                : <Menu/>}
         </div>
     );
 }
