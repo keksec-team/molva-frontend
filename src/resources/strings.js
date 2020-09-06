76fb90bb1ab578b21af6e33a47e7f3c2a6d4f79a
iamnzrv <nzrvofficial@gmail.com>
2020-09-06 21:06:57 +0300

    added about layout

--- src/resources/strings.js
+++ src/resources/strings.js
@@ -6,7 +6,18 @@ export let strings = {
         about: "О нас",
         contacts: "Контакты",
         englishLocale: "en",
-        russianLocale: "ru"
+        russianLocale: "ru",
+        aboutDescription: "Мы превращаем ваши идеи в реальные проекты",
+        aboutParagraph: "Для нас это дело - это не просто работа." +
+            " Мы собрали людей из разных областей творчества и программирования, чтобы вместе воплотить фантазии в жизнь. " +
+            "У вас есть идея проекта и вы ищете команду, чтобы реализовать ее? Тут и появляемся мы." +
+            " Наша команда создает полноценное визуальное решение и профессианально подходит к разработке каждой стадии проекта." +
+            " Мы работаем в области производства цифрового контента - снимаем видео, делаем 3D визуализации и создаем сайты с нуля.",
+        contactUs: "Связаться с нами",
+        ourProjects: "Наши проекты",
+        codeTitle: "Код",
+        gamesTitle: "Игры",
+        productTitle: "Продукт"
     },
     english: {
         home: "Home",
@@ -15,6 +26,17 @@ export let strings = {
         about: "About",
         contacts: "Contacts",
         englishLocale: "en",
-        russianLocale: "ru"
+        russianLocale: "ru",
+        aboutDescription: "We turn your ideas into great projects",
+        aboutParagraph: "Your idea is more than a job for us." +
+            " We brought people from different areas of art and software engineering to turn your ideas into reality. " +
+            "Do you have an idea and look for a team to make it a project? That's when we come in." +
+            " Our team creates a comprehensive visualisation and professionally develops every stage of your project." +
+            " We work in the field of producing the digital content - shooting videos, making 3D visualisations and creating websites from scratch.",
+        contactUs: "Contact us",
+        ourProjects: "Our projects",
+        codeTitle: "Code",
+        gamesTitle: "Games",
+        productTitle: "Product"
     }
 }
